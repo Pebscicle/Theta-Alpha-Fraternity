e3cbd132d2ccb600d8c9a71227493bd8703c2689
Pebscicle <pebscicle@gmail.com>
2022-07-22 22:27:07 +0200

    fix2

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="Theta-Alpha-Fraternity" element={<Home/>} />
           <Route path="brothers" element={<Brothers />} />
           <Route path="events" element={<Events />} />
           <Route path="contact" element={<Contact />} />
